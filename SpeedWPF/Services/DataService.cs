38ce139172dda4755a6252807300ec2bb077b292
Infarh <shmachilin@yandex.ru>
2020-02-07 14:16:40 +0300

    Запуск/остановка обновления данных в сервисе

--- SpeedWPF/Services/DataService.cs
+++ SpeedWPF/Services/DataService.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using SpeedWPF.Services.Interfaces;
 
 namespace SpeedWPF.Services
 {
     public class DataService : IDataService
     {
+        public class DataUpdatedEventArgs : EventArgs
+        {
+            public byte[] Data { get; }
+
+            public DataUpdatedEventArgs(byte[] Data) => this.Data = Data;
+        }
+
         private const int __DataCount = 512;
         private const int __DataLength = 128;
         private readonly byte[][] _Data;
+
+        private int _CurrentDataIndex = 0;
+
+        public event EventHandler<DataUpdatedEventArgs> DataUpdated;
 
         public DataService()
         {
@@ -28,6 +41,29 @@ namespace SpeedWPF.Services
                     var value = Math.Sin(2 * Math.PI * ((double)j / __DataLength + (double)i / __DataCount));
                     BitConverter.TryWriteBytes(data_location, value);
                 }
+            }
+        }
+
+        private CancellationTokenSource _DataUpdateCancellation;
+        public void StartDataUpdate(TimeSpan Timeout)
+        {
+            var cancellation = new CancellationTokenSource();
+            Interlocked.Exchange(ref _DataUpdateCancellation, cancellation)?.Cancel();
+            UpdateDataAsync(Timeout, cancellation.Token);
+        }
+
+        public void StopDataUpdate() => _DataUpdateCancellation?.Cancel();
+
+        // ReSharper disable once FunctionNeverReturns
+        private async void UpdateDataAsync(TimeSpan Timeout, CancellationToken Cancel)
+        {
+            while (true)
+            {
+                await Task.Delay(Timeout, Cancel).ConfigureAwait(false);
+                Cancel.ThrowIfCancellationRequested();
+                _CurrentDataIndex = (_CurrentDataIndex + 1) % __DataLength;
+                var data = _Data[_CurrentDataIndex];
+                DataUpdated?.Invoke(this, new DataUpdatedEventArgs(data));
             }
         }
     }
